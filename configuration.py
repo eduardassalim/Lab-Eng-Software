8243e4fc1fb1b83d32a584e1a4e9e9f3056b7d0c
Arthur Vaz <pv.arthur10@gmail.com>
2024-11-14 19:53:37 -0300

    Criado arquivo para configs do app

---
+++ configuration.py
@@ -0,0 +1,29 @@
+from database.database import db # importação do banco de dados
+
+from routes.home import home_route # importação da blueprint home
+from routes.cliente import cliente_route # importação da blueprint cliente
+from routes.atendente import atendente_route # importação da blueprint atendente
+from routes.editora import editora_route # importação da blueprint editora
+from routes.livro import livro_route # importação da blueprint editora
+
+# importação das models das tabelas do banco (abaixo)
+from database.models.livro import Livro
+from database.models.cliente import Cliente
+from database.models.editora import Editora
+from database.models.atendente import Atendente
+
+def configure_all(app):
+    configure_db()
+    configure_routes(app)
+
+def configure_routes(app):
+    app.register_blueprint(home_route)
+    app.register_blueprint(cliente_route, url_prefix='/clientes')
+    app.register_blueprint(atendente_route, url_prefix='/atendentes')
+    app.register_blueprint(editora_route, url_prefix='/editoras')
+    app.register_blueprint(livro_route, url_prefix='/livros')
+
+
+def configure_db():
+    db.connect() # conexão com o banco de dados
+    db.create_tables([Cliente, Atendente, Livro, Editora]) # criação das tabelas do banco de dados (caso as tabelas já existirem ele irá carrega-las)
--- main.py
+++ main.py
@@ -1,32 +1,9 @@
-from flask import Flask, render_template, request, redirect # importação dos métodos Flask utilizados no código
-
-from datetime import datetime # importação do método datetime
-
-from routes.home import home_route # importação da blueprint home
-from routes.cliente import cliente_route # importação da blueprint cliente
-from routes.atendente import atendente_route # importação da blueprint atendente
-from routes.editora import editora_route # importação da blueprint editora
-from routes.livro import livro_route # importação da blueprint editora
-
-# importação das models das tabelas do banco (abaixo)
-from database.models.livro import Livro
-from database.models.cliente import Cliente
-from database.models.editora import Editora
-from database.models.atendente import Atendente
-
-from database.database import db # importação do banco de dados
+from flask import Flask # importação do método Flask
+from configuration import configure_all
 
 app = Flask(__name__) # inicialização padrão do aplicativo Flask
 
-app.register_blueprint(home_route)
-app.register_blueprint(cliente_route, url_prefix='/clientes')
-app.register_blueprint(atendente_route, url_prefix='/atendentes')
-app.register_blueprint(editora_route, url_prefix='/editoras')
-app.register_blueprint(livro_route, url_prefix='/livros')
-
-db.connect() # conexão com o banco de dados
-
-db.create_tables([Cliente, Atendente, Livro, Editora]) # criação das tabelas do banco de dados (caso as tabelas já existirem ele irá carrega-las)
+configure_all(app)
 
 # verifica se o programa está sendo rodado no main e roda a aplicação Flask
 if (__name__ == '__main__'):
